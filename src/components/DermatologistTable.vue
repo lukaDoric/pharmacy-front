a69cf1f7fd7d647f6b425fe3c3a711bc8367454e
lukaDoric <luka.doric@uns.ac.rs>
2021-01-12 13:28:30 +0100

    NavBar and Routing added.

---
+++ src/components/DermatologistTable.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-jumbotron class="jumbotron">
+    <h1 class="display-4">Dermatologists</h1>
+    <div>
+      <table class="table table-dark">
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </b-jumbotron>
+</template>
+
+<script>
+export default {
+  name: "DermatologistTable"
+}
+</script>
+
+<style>
+.jumbotron {
+  border-radius: 5em !important;
+  margin-right: 5em;
+  margin-left: 5em;
+  margin-top: 5em;
+}
+
+.display-4 {
+  margin-bottom: 0.5em;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link to="/"><a class="navbar-brand">Sputnik Pharmacy</a></router-link>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link to="/dermatologists"><a class="nav-link" href="#">Our dermatologists<span class="sr-only">(current)</span></a>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    msg: String
+  }
+}
+</script>
