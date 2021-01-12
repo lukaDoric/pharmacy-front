d31bb8db1388b4bcf67bc86c2cb626fca81f3191
lukaDoric <luka.doric@uns.ac.rs>
2021-01-12 19:55:05 +0100

    Pharmacy contact added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueLayers from 'vuelayers'
 import 'vuelayers/lib/style.css' // needs css-loader
+import {PointGeom} from 'vuelayers'
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -14,7 +16,8 @@ import Contact from "@/components/Contact/Contact"
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 Vue.use(VueRouter);
-Vue.use(VueLayers)
+Vue.use(VueLayers);
+Vue.use(PointGeom);
 
 const routes = [
     {path: '/ourteam', component: OurTeam},
