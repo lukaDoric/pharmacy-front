b1b689721b7d2833c547003b25b2be43e5fc1f83
lukaDoric <luka.doric@uns.ac.rs>
2021-01-21 13:59:51 +0100

    User is able to filter pharmacists

--- src/components/OurTeam/Pharmacists.vue
+++ src/components/OurTeam/Pharmacists.vue
@@ -1,50 +1,116 @@
 <template>
   <b-jumbotron class="jumbotron">
     <h1 class="display-4">Pharmacists</h1>
-    <div>
-      <table class="table table-dark">
-        <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">Name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Appointment</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>
-            <button type="button" class="btn btn-success">Check availability</button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>
-            <button type="button" class="btn btn-success">Check availability</button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>
-            <button type="button" class="btn btn-success">Check availability</button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+
+    <div class="container my-4">
+      <hr>
+      <div class="table-responsive">
+        <div class="d-flex bd-highlight mb-3">
+          <input type="text"
+                 placeholder="Search"
+                 v-model="filter">
+          <div>
+            <select v-on:select="filterByPharmacy" id="dropdown-1" text="Pharmacy" class="m-md-2"
+                    @change="filterByPharmacy($event)">
+              <option>All pharmacies</option>
+              <option v-for="(pharmacy, index) in pharmacies" :key="index">{{ pharmacy.pharmacyName }}</option>
+            </select>
+          </div>
+        </div>
+        <table class="table">
+          <thead>
+          <tr>
+            <th scope="col">See pharamcy</th>
+            <th scope="col">Name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Rating</th>
+          </tr>
+          </thead>
+          <tbody v-for="(pharmacist, index) in filteredPharmacists" :key="index">
+          <tr class="accordion-toggle collapsed" id="accordion1" data-toggle="collapse" data-parent="#accordion1"
+              href="#collapseOne">
+            <td class="expand-button"></td>
+            <td>{{ pharmacist.name }}</td>
+            <td>{{ pharmacist.surname }}</td>
+            <td>{{ pharmacist.rating }}</td>
+          </tr>
+          <tr class="hide-table-padding">
+            <td></td>
+            <td colspan="3">
+              <div id="collapseOne" class="collapse in p-3">
+                <div class="row" v-for="(pharmacy, index) in pharmacist.pharmacies" :key="index">
+                  <div class="col-3"><p style="color:forestgreen;">{{ pharmacy.pharmacyName }}</p></div>
+                  <div class="col-4"><p style="color:forestgreen;">{{ pharmacy.pharmacyAddress }}</p></div>
+                </div>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
     </div>
   </b-jumbotron>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Pharmacists"
+  name: "Dermatologists",
+  data() {
+    return {
+      filter: '',
+      filterByPharmacyValue: '',
+      pharmacies: [],
+      pharmacists: [],
+      allPharmacists: []
+    }
+  },
+
+  mounted() {
+    axios
+        .get('http://localhost:8080/user/getAllPharmacists')
+        .then(response => {
+          this.pharmacists = response.data;
+          this.allPharmacists = response.data;
+        })
+
+    axios
+        .get('http://localhost:8080/pharmacy/')
+        .then(response => {
+          this.pharmacies = response.data;
+        })
+  },
+
+  methods: {
+
+    filterByPharmacy(event) {
+      console.log(event.target.value);
+
+      if (event.target.value === 'All pharmacies') {
+        this.pharmacists = this.allPharmacists;
+      } else {
+        axios
+            .get('http://localhost:8080/pharmacist/getPharmacistsByPharmacy/' + event.target.value)
+            .then(response => {
+              this.pharmacists = response.data;
+            })
+      }
+    }
+  },
+
+  computed: {
+    filteredPharmacists() {
+      return this.pharmacists.filter(pharmacist => {
+        const name = pharmacist.name.toString().toLowerCase();
+        const surname = pharmacist.surname.toLowerCase();
+        const rating = pharmacist.rating.toString();
+        const searchTerm = this.filter.toLowerCase();
+        return name.includes(searchTerm) || surname.includes(searchTerm) || rating.includes(searchTerm);
+      });
+    }
+  },
 }
 </script>
 
@@ -58,5 +124,25 @@ export default {
 
 .display-4 {
   margin-bottom: 0.5em;
+}
+
+tr.hide-table-padding td {
+  padding: 0;
+}
+
+.expand-button {
+  position: relative;
+}
+
+.accordion-toggle .expand-button:after {
+  position: absolute;
+  left: .75rem;
+  top: 50%;
+  transform: translate(0, -50%);
+  content: '-';
+}
+
+.accordion-toggle.collapsed .expand-button:after {
+  content: '+';
 }
 </style>
