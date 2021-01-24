20cd1025f35660a0919575ef4a720dc1edb48e60
lukaDoric <luka.doric@uns.ac.rs>
2021-01-24 18:57:45 +0100

    Admin can accept/reject vaction request from pharmacist.

--- src/components/VacationRequest/VacationRequests.vue
+++ src/components/VacationRequest/VacationRequests.vue
@@ -9,7 +9,7 @@
             <th scope="col">Name</th>
             <th scope="col">Surname</th>
             <th scope="col">Vacation Time</th>
-            <th scope="col">Accept/Decline</th>
+            <th scope="col">Accept/Reject</th>
           </tr>
           </thead>
           <tbody v-for="(vacation, index) in pharmacistVacations" :key="index">
@@ -18,16 +18,22 @@
             <td>{{ vacation.surname }}</td>
             <td>{{ vacation.vacationInterval }}</td>
             <td>
-              <form>
-                <input v-model="selected[index]" type="radio" name="status" value="accept">
+              <form v-if="pharmacistVacations[index].status === 'Waiting for response'">
+                <input v-model="pharmacistApproved[index]" type="radio" name="status" value=true>
                 <label>Accept</label><br>
-                <input v-model="selected[index]" type="radio" name="status" value="decline">
+                <input v-model="pharmacistApproved[index]" type="radio" name="status" value=false>
                 <label>Decline</label><br>
               </form>
-              <b-button class="btn-success" @click="sendVacationResponsePharmacist(index)">Send</b-button>
+              <p v-if="pharmacistVacations[index].approved">APPROVED</p>
+              <p v-else-if="!pharmacistVacations[index].approved && pharmacistVacations[index].status === 'Responded'">
+                REJECTED</p>
+              <b-button v-if="pharmacistVacations[index].status === 'Waiting for response'" class="btn-success"
+                        @click="sendVacationResponsePharmacist(index)">Send
+              </b-button>
             </td>
           </tr>
-          <b-form-textarea v-model="reason[index]" placeholder="Reason..." v-if="selected[index] === 'decline'">
+          <b-form-textarea v-model="pharmacistReason[index]" placeholder="Reason..."
+                           v-if="pharmacistApproved[index] === 'false'">
           </b-form-textarea>
           </tbody>
         </table>
@@ -53,15 +59,17 @@
             <td>{{ vacation.vacationInterval }}</td>
             <td>
               <form>
-                <input v-model="selected[index]" type="radio" name="status" value="accept">
+                <input v-model="dermatologistApproved[index]" type="radio" name="status" value=true>
                 <label>Accept</label><br>
-                <input v-model="selected[index]" type="radio" name="status" value="decline">
+                <input v-model="dermatologistApproved[index]" type="radio" name="status" value=false>
                 <label>Decline</label><br>
               </form>
-              <b-button class="btn-success" @click="sendVacationResponseDermatologist(index)">Send</b-button>
+              <b-button class="btn-success" @click="sendVacationResponseDermatologist(index)">Send
+              </b-button>
             </td>
           </tr>
-          <b-form-textarea rowspan="4" v-model="reason[index]" placeholder="Reason..." v-if="selected[index] === 'decline'">
+          <b-form-textarea rowspan="4" v-model="dermatologistReason[index]" placeholder="Reason..."
+                           v-if="dermatologistApproved[index] === 'false'">
           </b-form-textarea>
           </tbody>
         </table>
@@ -76,10 +84,14 @@ import axios from "axios";
 export default {
   data() {
     return {
-      reason: [],
-      selected: [],
+      pharmacistReason: [],
+      dermatologistReason: [],
+      pharmacistApproved: [],
+      dermatologistApproved: [],
       pharmacistVacations: [],
-      dermatologistVacations: []
+      dermatologistVacations: [],
+      pharmacistVacationStatus: [],
+      dermatologistVacationStatus: []
     }
   },
 
@@ -88,6 +100,9 @@ export default {
         .get('http://localhost:8080/vacation/getAllPharmacistsVacation')
         .then(response => {
           this.pharmacistVacations = response.data;
+          this.pharmacistVacations.forEach(vacation => {
+            this.pharmacistVacationStatus.add(vacation.status)
+          });
         })
 
     axios
@@ -98,12 +113,37 @@ export default {
   },
 
   methods: {
-    sendVacationResponseDermatologist(index) {
-      console.log(this.reason[index])
-    },
+    // sendVacationResponseDermatologist(index) {
+    //   axios
+    //       .post('http://localhost:8080/vacation/sendVacationResponseDermatologist',
+    //           {id: this.pharmacistVacations[index].id,
+    //           name: this.pharmacistVacations[index].name,
+    //           surname: this.pharmacistVacations[index].surname,
+    //           vacationInterval: this.pharmacistVacations[index].vacationInterval,
+    //           role: this.pharmacistVacations[index].role,
+    //           isApproved: this.dermatologistApproved[index],
+    //           reason: this.dermatologistReason[index]})
+    // },
 
     sendVacationResponsePharmacist(index) {
-      console.log(this.reason[index])
+
+      let approved = 'true';
+
+      if (this.pharmacistApproved[index] === 'false') {
+        approved = 'false';
+      }
+
+      axios
+          .post('http://localhost:8080/vacation/sendVacationResponsePharmacist/',
+              {
+                id: this.pharmacistVacations[index].id,
+                name: this.pharmacistVacations[index].name,
+                surname: this.pharmacistVacations[index].surname,
+                vacationInterval: this.pharmacistVacations[index].vacationInterval,
+                role: this.pharmacistVacations[index].role,
+                approved: approved,
+                reason: this.pharmacistReason[index]
+              }).then(window.location.reload());
     }
   }
 }
