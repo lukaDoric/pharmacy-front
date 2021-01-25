eebb87eb0dba5f75f7f509c5e705c8f5c2ccdfaf
Vladimir Budjen <bvalda88@gmail.com>
2021-01-25 23:24:18 +0100

    Medicine reservation page changed for search and filter.

--- src/components/Medicine/Medicine.vue
+++ src/components/Medicine/Medicine.vue
@@ -1,68 +1,131 @@
 <template>
   <div>
-    <b-jumbotron>
-      <h1 class="display-4">Medicine</h1>
-      <table class="table table-dark table-hover table-bordered">
-        <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          <th scope="col">Form</th>
-        </tr>
-        </thead>
-        <tbody v-for="(m, index) in medicine" v-bind:key="m.id">
-        <tr class="align-content-center">
-          <th scope="row">{{ index }}</th>
-          <td class="align-baseline">{{ m.name }}</td>
-          <td class="align-baseline">{{ m.description }}</td>
-          <td class="align-baseline">{{ m.form }}</td>
-          <td class="align-baseline">
-            <button type="button" class="btn btn-info" v-on:click="onClick(m)">Make a reservation</button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+    <div class="row">
+      <div class="col-lg-7">
+        <b-jumbotron class="jumbotron">
+          <h1 class="display-4">Medicine</h1>
+          <table class="table table-dark table-hover table-bordered">
+            <thead>
+            <tr>
+              <th scope="col"><button class="btn btn-info btn-sm" type="button">search</button></th>
+              <th scope="col">Name
+                <input size="10" type="text">
+              </th>
+              <th scope="col">Type
+                <select>
+                  <option>All</option>
+                  <option v-for="key in enumTypeDict" v-bind:key="key" class="dropdown-item">{{ key }}</option>
+                </select>
+              </th>
+              <th scope="col">Form
+                <select>
+                  <option>All</option>
+                  <option v-for="key in enumFormDict" v-bind:key="key" class="dropdown-item">{{ key }}</option>
+                </select>
+              </th>
+              <th scope="col">Rating
 
-      <h2 v-if="selectedMedicine != null" class="display-4">Make a reservation</h2>
-
-      <div class="container bg-dark p-5" v-if="selectedMedicine != null">
-        <p class="text-light text-left">By submitting this reservation you are confirming that you will purchase the
-          medicine at the selected pharmacy by the date chosen.<br>You can cancel the reservation up to 24 hours before
-          it expires to avoid penalty points.</p>
-        <form v-on:submit.prevent="onSubmit">
-          <div class="form-row">
-            <div class="col">
-              <input type="text" class="form-control" :readonly="true" v-model="selectedMedicine.name"
-                     placeholder="Medicine">
-            </div>
-            <div class="col">
-              <input type="date" class="form-control" v-model="expirationDate" placeholder="Reservation expires">
-            </div>
-          </div>
-          <div class="form-group mt-1 p-1" style="width: 100%">
-            <div class="dropdown">
-              <button class="btn btn-info dropdown-toggle btn-block" type="button" id="dropdownMenuButton"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{dropdownText}}
-              </button>
-              <div class="dropdown-menu btn-block" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" v-on:click.prevent="onSelectionChanged(p)" v-for="p in pharmacies"
-                   v-bind:key="p.id">{{ p.name + ", " + p.address }}</a>
-              </div>
-            </div>
-          </div>
-          <div class="form-group mt-1" style="width: 100%">
-            <button type="submit" class="btn btn-info btn-block">Submit reservation
-            </button>
-          </div>
-        </form>
+                <select>
+                  <option>All</option>
+                  <option v-for="index in 5" v-bind:key="index">{{ index }}</option>
+                </select>
+              </th>
+              <th scope="col">Generate PDF</th>
+              <th scope="col">Select</th>
+            </tr>
+            </thead>
+            <tbody v-for="(m, index) in medicine" v-bind:key="m.id">
+            <tr class="align-content-center accordion-toggle collapsed">
+              <th scope="row">{{ index }}</th>
+              <td class="align-baseline">{{ m.name }}</td>
+              <td class="align-baseline">{{ typeFilter(m.type) }}</td>
+              <td class="align-baseline">{{ formFilter(m.form) }}</td>
+              <td class="align-baseline">{{ m.ratings }}</td>
+              <td>
+                <button type="button" class="btn btn-info btn-sm" v-on:click="downloadPdf(m)">Download</button>
+              </td>
+              <td class="align-baseline" >
+                <button type="button" class="btn btn-info btn-sm" v-on:click="selectMedicine(m)">+</button>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </b-jumbotron>
       </div>
-
-    </b-jumbotron>
+      <div class="col-lg-5">
+        <b-jumbotron class="jumbotron">
+          <div class="card">
+            <div class="card-header">
+              <h2 v-if="selectedMedicine != null && loggedUser!=null" class="display-4">Make {{ selectedMedicine.name }} reservation</h2>
+              <h2 v-else-if="selectedMedicine != null && loggedUser==null" class="display-4">{{ selectedMedicine.name }}' pharmacies</h2>
+              <h2 v-else-if="loggedUser==null" class="display-4">Select medicine to see it's pharmacies.</h2>
+              <h2 v-else class="display-4">Select medicine to make a reservation.</h2>
+            </div>
+            <div class="card-body bg-dark" v-if="selectedMedicine != null">
+                <table class="table table-dark table-hover table-bordered" v-if="selectedMedicine != null">
+                  <thead>
+                  <tr>
+                    <th>Pharmacy name</th>
+                    <th>Pharmacy address</th>
+                    <th>Price</th>
+                    <th v-if="loggedUser!=null"></th>
+                  </tr>
+                  </thead>
+                  <tbody v-for="p in pharmacies" v-bind:key="p.id">
+                  <tr>
+                    <td>{{p.name}}</td>
+                    <td>{{p.address}}</td>
+                    <td>dolary</td>
+                    <td v-if="loggedUser!=null"><button class="btn btn-info btn-sm" v-on:click="selectedPharmacy=p">select</button></td>
+                  </tr>
+                  </tbody>
+                </table>
+                <h4 v-if="pharmacies.length==0 && selectedMedicine != null" style="color:white">
+                  No pharmacies were found.
+                </h4>
+                <div v-if="selectedPharmacy!=null" style="color: white">
+                  <hr color="white">
+                  <div class="form-row">
+                    <p>Medicine: {{selectedMedicine.name}}<br></p>
+                  </div>
+                  <div class="form-row">
+                    <p>Pharmacy: {{selectedPharmacy.name}}</p>
+                  </div>
+                  <div class="form-row">
+                    <div class="col">
+                      <input type="date" class="form-control" v-model="expirationDate" placeholder="Reservation expires">
+                    </div>
+                  </div>
+                  <div class="form-group mt-1" style="width: 100%">
+                    <button type="submit" v-on:click="onSubmit" class="btn btn-info btn-block">Submit reservation
+                    </button>
+                  </div>
+                </div>
+            </div>
+            <div class="card-footer bg-dark" v-if="selectedMedicine != null && loggedUser!=null"><small class="text-light text-left">By submitting this reservation you are
+              confirming that you will purchase
+              the
+              medicine at the selected pharmacy by the date chosen.<br>You can cancel the reservation up to 24 hours
+              before
+              it expires to avoid penalty points.</small>
+            </div>
+          </div>
+        </b-jumbotron>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <style scoped>
+#search-name-button {
+  margin: 5px;
+}
+
+.jumbotron {
+  margin: 2%;
+}
+
 </style>
 
 <script>
@@ -72,12 +135,31 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 export default {
   data() {
     return {
+      nameInput: "",
       medicine: [],
       pharmacies: [],
       selectedMedicine: null,
       selectedPharmacy: null,
       expirationDate: "",
-      dropdownText: "Select pharmacy"
+      dropdownText: "Select pharmacy",
+      enumTypeDict: {
+        'HUMAN': "Human medicament",
+        'HERBAL': "Herbal medicine",
+        'BIOPHARMACEUTICAL': "Biopharmaceutical",
+        'RADIOPHARMACEUTICAL': "Radiopharmaceutical",
+        'HOMEOPATHIC': "Homeopathic remedy"
+      },
+      enumFormDict: {
+        'TABLET': 'Tablet',
+        'SYRUP': 'Syrup',
+        'POWDER': 'Powder',
+        'CAPSULE': 'Capsule',
+        'INHALER': 'Inhaler',
+        'INJECTION': 'Injection',
+        'DROPS': 'Drops',
+        'GEL': 'Gel',
+        'PASTE': 'Paste'
+      }
     }
   },
   name: "Medicine",
@@ -87,11 +169,15 @@ export default {
         .then(response => (this.medicine = response.data))
   },
   methods: {
-    onClick(medicine) {
+    selectMedicine(medicine) {
+      this.selectedPharmacy = null;
       this.selectedMedicine = medicine
       axios
           .get("http://localhost:8080/medicine-reservation/pharmacies/" + medicine.id.toString())
           .then(response => (this.pharmacies = response.data))
+    },
+    downloadPdf(medicine){
+      console.log(medicine);
     },
     onSelectionChanged(pharmacy) {
       this.selectedPharmacy = pharmacy
@@ -113,11 +199,22 @@ export default {
             console.log(error.message)
             alert("Sorry, looks like there has been a mistake with your reservation. Please try again.")
           })
+    },
+    search() {
+      this.$router.push('/medicineSearched/' + this.nameInput);
+    },
+    formFilter: function (value) {
+      return this.enumFormDict[value];
+    },
+    typeFilter: function (value) {
+      return this.enumTypeDict[value];
+    },
+  },
+  computed:{
+    loggedUser(){
+      return this.$store.state.userType;
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
