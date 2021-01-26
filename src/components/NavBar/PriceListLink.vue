74a136c8c366da15c165bbc67a314c4f22f96601
lukaDoric <luka.doric@uns.ac.rs>
2021-01-26 23:42:41 +0100

    Admin is able to see current medicine prices, and define new price with time interval

---
+++ src/components/NavBar/PriceListLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="nav-item active">
+    <router-link to="/priceList"><a class="nav-link" href="#">Price List<span class="sr-only">(current)</span></a>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "PriceListLink"
+}
+</script>
+
+<style scoped>
+
+</style>
