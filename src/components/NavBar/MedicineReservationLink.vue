94e06316fd85647c2f9d8f247b134bcbcf664a5c
Milica Siriski <milica.siriski998@gmail.com>
2021-01-30 15:38:42 +0100

    Registered users can see their medicine reservations.

---
+++ src/components/NavBar/MedicineReservationLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="nav-item active">
+    <router-link to="/medicineReservation"><a class="nav-link" href="#">Medicine reservations<span class="sr-only">(current)</span></a>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+name: "MedicineReservationLink"
+}
+</script>
+
+<style scoped>
+
+</style>
