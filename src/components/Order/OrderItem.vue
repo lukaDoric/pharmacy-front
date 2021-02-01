f81983a29cbc31e1e5896872bc97ada8efa00b42
lukaDoric <luka.doric@uns.ac.rs>
2021-02-01 18:30:19 +0100

    Refactoring order functions for pharmacy admin

--- src/components/Order/OrderItem.vue
+++ src/components/Order/OrderItem.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <div class="form-row">
-      <div class="form-group col-md-6">
-        <input :value="medicine" class="form-control" @input="changeMedicineName" placeholder="Enter medicine name"
-               type="text">
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">{{ medicineName }}
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#" v-for="(medicine, index) in medicines"
+             :key="index" v-on:click="onMedicineSelected(medicine)">{{ medicine.name + '--' + medicine.form }}</a>
+        </div>
       </div>
+
       <div class="form-group col-md-6">
         <input :value="amount" class="form-control" @input="changeAmount" placeholder="Amount" type="number">
       </div>
@@ -17,12 +23,13 @@
 export default {
   name: "OrderItem",
 
-  props: ['medicine', 'amount'],
+  props: ['medicine', 'amount', 'medicines'],
 
   data() {
     return {
-      medicineName: '123',
-      amountValue: 0
+      medicineName: 'Select medicine',
+      amountValue: 0,
+      medicinesValue: [],
     }
 
   },
@@ -36,6 +43,11 @@ export default {
     changeAmount(event) {
       this.amountValue = event.target.value;
       this.$emit('amountChanged', this.amountValue);
+    },
+
+    onMedicineSelected(medicine) {
+      this.medicineName = medicine.name + '--' + medicine.form;
+      this.$emit('medicineIdChanged', medicine.id);
     }
   }
 }
