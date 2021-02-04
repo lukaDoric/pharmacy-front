634a63829f46418557a4be58ded23c573d2605ad
lukaDoric <luka.doric@uns.ac.rs>
2021-02-04 12:08:24 +0100

    Pharmacy admin is able to choose offer.

--- src/components/Order/Order.vue
+++ src/components/Order/Order.vue
@@ -28,8 +28,9 @@
       <h1 class="display-4">Recent orders</h1>
       <div v-for="(order, index) in recentOrders" :key="index">
         <div class="d-flex bd-highlight mb-3">
-          <button type="button" class="btn btn-success mt-3 mr-2">Check details</button>
+          <button type="button" class="btn btn-danger mt-3 mr-2" @click="removeOrder(order.id)">Remove order</button>
           <h4 class="mt-3 ml-5">Deadline: {{ order.deadlineString }}</h4>
+          <h4 class="mt-3 ml-5">Order id: {{ order.id }}</h4>
         </div>
         <table class="table table-dark">
           <thead>
@@ -45,6 +46,23 @@
           </tr>
           </tbody>
         </table>
+      </div>
+    </b-jumbotron>
+
+    <b-jumbotron class="jumbotron">
+      <h1 class="display-4">Offers</h1>
+      <div v-for="(offers, index) in offersList" :key="index">
+        <hr color="black">
+        <div class="d-flex bd-highlight mb-3" v-for="(offer, index) in offers" :key="index">
+          <button v-if="offer.status === 'WAITING'" type="button" class="btn btn-success mt-3 mr-2"
+                  @click="acceptOffer(offer.offerId)">Accept offer
+          </button>
+          <h4 class="mt-3 ml-5">Order Deadline: {{ formatDate(offer.orderDeadline) }}</h4>
+          <h4 class="mt-3 ml-5">Order id: {{ offer.orderId }}</h4>
+          <h4 class="mt-3 ml-5">Price: {{ offer.price + "$" }}</h4>
+          <h4 class="mt-3 ml-5">Shipping days: {{ offer.shippingDays }}</h4>
+          <h4 class="mt-3 ml-5">Status: {{ offer.status }}</h4>
+        </div>
       </div>
     </b-jumbotron>
   </div>
@@ -66,6 +84,7 @@ export default {
       medicineAmount: null,
       recentOrders: [],
       medicines: [],
+      offersList: [],
       today: new Date().toISOString().split('T')[0]
     }
   },
@@ -75,6 +94,12 @@ export default {
         .get('http://localhost:8080/order/getOrdersByPharmacy')
         .then(response => {
           this.recentOrders = response.data;
+        })
+
+    axios
+        .get('http://localhost:8080/offer/getOffersByPharmacy')
+        .then(response => {
+          this.offersList = response.data;
         })
 
     this.$http.get('http://localhost:8080/medicine/get/all')
@@ -104,7 +129,28 @@ export default {
           .post('http://localhost:8080/order/', {
             orderItems: this.medicineAmount,
             deadline: this.deadline
-          });
+          }).then(window.location.reload());
+    },
+
+    removeOrder(orderId) {
+      this.$http
+          .delete('http://localhost:8080/order/deleteOrder/' + orderId)
+          .then(window.location.reload())
+          .catch(reason => alert(reason.message));
+    },
+
+    acceptOffer(offerId) {
+      this.$http
+          .put('http://localhost:8080/offer/acceptOffer/' + offerId)
+          .then(window.location.reload())
+          .catch(reason => alert(reason.message));
+    },
+
+    formatDate(date) {
+      const ye = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(date);
+      const mo = new Intl.DateTimeFormat('en', {month: 'short'}).format(date);
+      const da = new Intl.DateTimeFormat('en', {day: '2-digit'}).format(date);
+      return `${da}-${mo}-${ye}`
     },
 
     addOrderItem() {
@@ -114,9 +160,7 @@ export default {
     removeOrderItem() {
       this.items.pop()
     }
-
   }
-
 }
 </script>
 
