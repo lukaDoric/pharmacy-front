299ba42cca4854b6e9e54c293a7c870dd40c49e6
Milica Siriski <milica.siriski998@gmail.com>
2021-02-08 19:27:21 +0100

    Added geolocation to get user's current location. Some minor appearance changes.

--- src/components/Pharmacy/PharmacySearch.vue
+++ src/components/Pharmacy/PharmacySearch.vue
@@ -6,35 +6,31 @@
       <div class="input-group mb-3 px-5">
         <input type="text" class="form-control" placeholder="Search pharmacies" v-model="searchParam">
       </div>
-      <div class="row">
-        <div class="col">
-          <div class="input-group form-inline mb-3 px-5">
-            <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" type="button" id="pharmacistsSort"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {{ ratingDropdownText }}
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(0)">Any Rating</a>
-                <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(1)">1+ Star Rating</a>
-                <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(2)">2+ Star Rating</a>
-                <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(3)">3+ Star Rating</a>
-                <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(4)">4+ Star Rating</a>
-                <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(5)">5 Star Rating (Best)</a>
-              </div>
+      <div class="row mx-5">
+        <div class="input-group form-inline mb-3 px-5">
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="pharmacistsSort"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {{ ratingDropdownText }}
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(0)">Any Rating</a>
+              <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(1)">1+ Star Rating</a>
+              <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(2)">2+ Star Rating</a>
+              <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(3)">3+ Star Rating</a>
+              <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(4)">4+ Star Rating</a>
+              <a class="dropdown-item" href="#" v-on:click="onRatingFilterChanged(5)">5 Star Rating (Best)</a>
             </div>
-            <div class="input-group-prepend">
-              <span class="input-group-text bg-light ml-4">Show results within</span>
-            </div>
-            <input type="number" min="1" class="form-control flex-fill" aria-label="Distance" v-model="distance">
-            <div class="input-group-append">
-              <span class="input-group-text bg-light mr-4">km</span>
-            </div>
-            <button class="btn btn-info" v-on:click="onFilterSearch()">Filter search</button>
           </div>
-        </div>
-        <div class="col">
-
+          <div class="input-group-prepend">
+            <span class="input-group-text bg-light ml-4">Show results within</span>
+          </div>
+          <input type="number" step="5" min="5" class="form-control" aria-label="Distance"
+                 v-model="distance">
+          <div class="input-group-append">
+            <span class="input-group-text bg-light mr-4">km</span>
+          </div>
+          <button class="btn btn-info" v-on:click="onFilterSearch()">Filter search</button>
         </div>
       </div>
 
@@ -57,6 +53,7 @@
 </template>
 
 <script>
+
 export default {
   name: "PharmacySearch",
   data() {
@@ -67,10 +64,23 @@ export default {
       ratingDropdownText: 'All Ratings',
       ratingFilterValues: ['NONE', 'ONE_STAR', 'TWO_STARS', 'THREE_STARS', 'FOUR_STARS', 'FIVE_STARS'],
       ratingFilter: 'NONE',
-      distance: 10
+      distance: 10,
+      userLon: 0.0,
+      userLat: 0.0
     }
   },
   mounted() {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+          this.userLat = position.coords.latitude;
+          this.userLon = position.coords.longitude;
+          console.log(this.userLon, this.userLat)
+        },
+        error => {
+          console.log(error.message);
+        },
+    )
+
     this.$http
         .get("http://localhost:8080/pharmacy/getAll/")
         .then(response => {
@@ -84,7 +94,7 @@ export default {
     },
     onFilterSearch() {
       this.$http
-          .get("http://localhost:8080/pharmacy/" + this.ratingFilter + "/" + this.distance)
+          .get("http://localhost:8080/pharmacy/" + this.ratingFilter + "/" + this.distance + "/" + this.userLon + "/" + this.userLat)
           .then(response => {
             this.pharmacies = response.data;
           })
