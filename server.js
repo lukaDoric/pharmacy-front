1adcfdff6839c2228e510d921089324e336158f9
lukaDoric <luka.doric@uns.ac.rs>
2021-02-11 13:13:29 +0100

    Heroku config

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const port = process.env.PORT || 8081;
+const serveStatic = require('serve-static')
+const path = require('path')
 const app = express();
 
-app.use(express.static(__dirname + "/dist/"));
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 app.get(/.*/, function (req, res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
-app.listen(port);
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
-console.log('Server started...')
+const port = process.env.PORT || 8081
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
